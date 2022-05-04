e036be858ded960945438cfaec22978618d4fc4c
tssibanda <tssibanda@hotmail.com>
2022-05-04 17:06:54 +0100

    fix environment

--- Server/server.js
+++ Server/server.js
@@ -42,7 +42,7 @@ app.use('/api/users', require('./Routes/userRoutes'))
 app.use('/api/stockanalyser', require('./Routes/stockAnalyerRoutes'))
 
 // serve frontend
-if(process.env.NODE_ENV === 'PROD'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '../client/build')))
     app.get('*', (req, res) => res.sendFile(
         path.resolve(__dirname, '../', 'client', 'build', 'index.html')
